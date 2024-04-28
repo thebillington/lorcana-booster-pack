b8a53c4ca857db1a6cd62a5e29247f1dfa4fb909
LewisDanks <lewisdanks@gmail.com>
2024-04-28 18:32:42 +0100

    client side with webpack and express server working

--- lorcana-learner/config/webpack.dev.js
+++ lorcana-learner/config/webpack.dev.js
@@ -1,16 +1,61 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: {
-        main: "./src/main.js"
+        main: "./src/main.ts"
     },
     output: {
         filename: "[name]-bundle.js",
         path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
+        publicPath: "/",
+        clean: true,
     },
     devServer: {
-        static: "dist"
-    }
-};
+        static: "dist",
+        client: {
+            overlay: true
+        }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  '@babel/preset-typescript'
+                ]
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            generator: {
+              filename: 'assets/[name][ext]'
+            }
+          },
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+        }),
+      ],
+      resolve: {
+        extensions: ['.ts', '.js'],
+      },
+    };
