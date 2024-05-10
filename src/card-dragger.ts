a7dff9a7726554ae7cdeffcb434fd932e5fe80f2
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-05-10 08:36:28 +0100

    Format

--- src/card-dragger.ts
+++ src/card-dragger.ts
@@ -1,60 +1,59 @@
+import { Card } from './models/card';
+import { DeckPage } from './load-deck';
+import { BoosterPack } from './booster-pack';
 
-    import { Card } from './models/card';
-    import { DeckPage } from './load-deck';
-    import { BoosterPack } from './booster-pack';
-    
-    export class CardDragger {
-        private draggedCard: [number, Card] | undefined;
-        private deckPage: DeckPage;
-        private boosterPack: BoosterPack;
-    
-        constructor() {
-            this.deckPage = (window as any).deckpage as DeckPage;
-            this.boosterPack = (window as any).boosterPack as BoosterPack;
-        }
-    
-        setupDragging() {
-            const cardElements = document.querySelectorAll('.card') as NodeListOf<HTMLElement>;
-    
-            cardElements.forEach(cardElement => {
-                const isCardDraggable = cardElement.draggable === true;
-                cardElement.addEventListener('dragstart', this.handleDragStart.bind(this), false);
-                cardElement.addEventListener('dragend', () => this.handleDragEnd());
+export class CardDragger {
+    private draggedCard: [number, Card] | undefined;
+    private deckPage: DeckPage;
+    private boosterPack: BoosterPack;
 
-                const images = cardElement.querySelectorAll('img');
-                images.forEach(img => {
-                    if (!isCardDraggable) {
-                        img.addEventListener('dragstart', (event) => event.preventDefault());
-                    }
-                });
-            });
-        }
-    
-        private handleDragStart(event: DragEvent) {
-            const cardElement = this.findCardElement(event.target as HTMLElement);
-            if (!cardElement) return;
-        
-            const position = parseInt(cardElement.getAttribute('position') || '0');
-            this.draggedCard = [position, this.boosterPack.getCard(position)];
-        }
-    
-        private handleDragEnd() {
-            this.draggedCard = undefined;
-        }
-    
-        public handleDropIntoDeck(event: DragEvent) {
-            event.preventDefault();
-            if(!this.draggedCard) return;
-            this.deckPage.insertCard(this.draggedCard[1]!);
-            this.boosterPack.removeCard(this.draggedCard[0]!);
-        }
-
-        private findCardElement(element: HTMLElement | null): HTMLElement | null {
-            while (element && !element.classList.contains('card')) {
-                element = element.parentElement;
-            }
-            return element;
-        }
+    constructor() {
+        this.deckPage = (window as any).deckpage as DeckPage;
+        this.boosterPack = (window as any).boosterPack as BoosterPack;
     }
 
-    (window as any).cardDragger = new CardDragger();
+    setupDragging() {
+        const cardElements = document.querySelectorAll('.card') as NodeListOf<HTMLElement>;
+
+        cardElements.forEach(cardElement => {
+            const isCardDraggable = cardElement.draggable === true;
+            cardElement.addEventListener('dragstart', this.handleDragStart.bind(this), false);
+            cardElement.addEventListener('dragend', () => this.handleDragEnd());
+
+            const images = cardElement.querySelectorAll('img');
+            images.forEach(img => {
+                if (!isCardDraggable) {
+                    img.addEventListener('dragstart', (event) => event.preventDefault());
+                }
+            });
+        });
+    }
+
+    private handleDragStart(event: DragEvent) {
+        const cardElement = this.findCardElement(event.target as HTMLElement);
+        if (!cardElement) return;
+
+        const position = parseInt(cardElement.getAttribute('position') || '0');
+        this.draggedCard = [position, this.boosterPack.getCard(position)];
+    }
+
+    private handleDragEnd() {
+        this.draggedCard = undefined;
+    }
+
+    public handleDropIntoDeck(event: DragEvent) {
+        event.preventDefault();
+        if (!this.draggedCard) return;
+        this.deckPage.insertCard(this.draggedCard[1]!);
+        this.boosterPack.removeCard(this.draggedCard[0]!);
+    }
+
+    private findCardElement(element: HTMLElement | null): HTMLElement | null {
+        while (element && !element.classList.contains('card')) {
+            element = element.parentElement;
+        }
+        return element;
+    }
+}
+
+(window as any).cardDragger = new CardDragger();
--- src/load-deck.ts
+++ src/load-deck.ts
@@ -5,182 +5,182 @@ import { Exporter } from './exporter';
 import { CardDragger } from './card-dragger';
 
 const inkImages: { [id: string]: string } = {
-  'amber': require('./images/inks/amber.png'),
-  'emerald': require('./images/inks/emerald.png'),
-  'ruby': require('./images/inks/ruby.png'),
-  'sapphire': require('./images/inks/sapphire.png'),
-  'steel': require('./images/inks/steel.png'),
-  'amethyst': require('./images/inks/amethyst.png')
+    'amber': require('./images/inks/amber.png'),
+    'emerald': require('./images/inks/emerald.png'),
+    'ruby': require('./images/inks/ruby.png'),
+    'sapphire': require('./images/inks/sapphire.png'),
+    'steel': require('./images/inks/steel.png'),
+    'amethyst': require('./images/inks/amethyst.png')
 };
 
 export class DeckPage {
-  private cardDragger: CardDragger | undefined;
-  public cards: Card[] = [];
-  public selectedCard = -1;
+    private cardDragger: CardDragger | undefined;
+    public cards: Card[] = [];
+    public selectedCard = -1;
 
-  public loadDeckFromBase64(encodedString: string): void {
-    this.fetchData(encodedString)
-      .then((cards) => {
-        this.cards = cards;
-        this.renderCards(cards);
+    public loadDeckFromBase64(encodedString: string): void {
+        this.fetchData(encodedString)
+            .then((cards) => {
+                this.cards = cards;
+                this.renderCards(cards);
 
-        const container = document.getElementById('get-booster-button');
-        if (!container) return;
-        container.style.display = "inline-block";
-        
-        this.cardDragger = (window as any).cardDragger as CardDragger;
-        this.cardDragger.setupDragging();
-        const deckContainer = document.getElementById('deck');
-        if (deckContainer) {
-            deckContainer.addEventListener('dragover', (event) => this.handleDragOver(event));
-            deckContainer.addEventListener('drop', (event) => this.handleDrop(event));
+                const container = document.getElementById('get-booster-button');
+                if (!container) return;
+                container.style.display = "inline-block";
+
+                this.cardDragger = (window as any).cardDragger as CardDragger;
+                this.cardDragger.setupDragging();
+                const deckContainer = document.getElementById('deck');
+                if (deckContainer) {
+                    deckContainer.addEventListener('dragover', (event) => this.handleDragOver(event));
+                    deckContainer.addEventListener('drop', (event) => this.handleDrop(event));
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching and rendering data:', error);
+            });
+    }
+
+    private async fetchData(encodedDeck: string): Promise<Card[]> {
+        try {
+            const response = await fetch(`/api/deck?encodedString=${encodedDeck}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            return [];
         }
-      })
-      .catch(error => {
-        console.error('Error fetching and rendering data:', error);
-      });
-  }
-
-  private async fetchData(encodedDeck: string): Promise<Card[]> {
-    try {
-      const response = await fetch(`/api/deck?encodedString=${encodedDeck}`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return [];
-    }
-  }
-
-  private async renderCards(cards: Card[]): Promise<void> {
-    const container = document.getElementById('deck');
-    if (!container) {
-      console.error('Container not found');
-      return;
     }
 
-    container.innerHTML = '';
+    private async renderCards(cards: Card[]): Promise<void> {
+        const container = document.getElementById('deck');
+        if (!container) {
+            console.error('Container not found');
+            return;
+        }
 
-    cards.forEach( (card, i) => {
-      const cardElement = document.createElement('div');
-      cardElement.classList.add('card');
-      cardElement.classList.add('deck-card');
-      cardElement.setAttribute("position", i.toString());
+        container.innerHTML = '';
 
-      const imageElement = document.createElement('img');
-      imageElement.src = card.images.full;
-      imageElement.alt = card.fullName;
-      if (i == this.selectedCard) imageElement.classList.add('selected');
+        cards.forEach((card, i) => {
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card');
+            cardElement.classList.add('deck-card');
+            cardElement.setAttribute("position", i.toString());
 
-      const nameElement = document.createElement('p');
-      nameElement.textContent = card.fullName;
+            const imageElement = document.createElement('img');
+            imageElement.src = card.images.full;
+            imageElement.alt = card.fullName;
+            if (i == this.selectedCard) imageElement.classList.add('selected');
 
-      cardElement.appendChild(imageElement);
-      cardElement.appendChild(nameElement);
+            const nameElement = document.createElement('p');
+            nameElement.textContent = card.fullName;
 
-      container.appendChild(cardElement);
-    });
+            cardElement.appendChild(imageElement);
+            cardElement.appendChild(nameElement);
 
-    this.updateCardRules(cards);
-    this.updateCardCounts(cards.length);
-    Stats.drawCostGraph(cards);
-  }
+            container.appendChild(cardElement);
+        });
 
-  private updateCardRules(cards: Card[]): void {
-    const imageContainers = document.getElementsByClassName('ink-token') as HTMLCollectionOf<HTMLImageElement>;
-    const inkCountContainers = document.getElementsByClassName('ink-card-counter');
-    if (!imageContainers || !inkCountContainers) return;
-
-    const inks: string[] = this.getInks(cards);
-    let inkCounts: Record<string, number> = {};
-    cards.forEach( card => {
-      if (card.color in inkCounts) {
-        inkCounts[card.color] += 1;
-      } else {
-        inkCounts[card.color] = 1;
-      }
-    });
-
-    inks.forEach( (ink, i) => {
-      imageContainers[i]!.src = inkImages[ink.toLowerCase()];
-      imageContainers[i].setAttribute('title', ink);
-      imageContainers[i].setAttribute('ink', ink.toLowerCase());
-      inkCountContainers[i].innerHTML = `${inkCounts[ink]}`;
-    });
-  }
-
-  private getInks(cards: Card[]): string[] {
-    const inks: string[] = [];
-    cards.forEach((card: Card) => {
-      if (inks.indexOf(card.color) == -1) {
-        inks.push(card.color);
-      }
-    });
-
-    return inks;
-  }
-
-  private handleDrop(event: DragEvent) {
-    this.cardDragger!.handleDropIntoDeck(event); 
-  }
-
-  private handleDragOver(event: DragEvent) {
-    event.preventDefault();
-  }
-
-  public insertCard(card: Card) {
-    this.cards.push(card);
-    this.renderCards(this.cards);
-  }
-
-  public getCard( position: number ): Card {
-    const card = this.cards.splice(position, 1)[0];
-    this.renderCards(this.cards);
-    return card;
-  }
-
-  private updateCardCounts( numCards: number ): void {
-    const container = document.getElementById('card-count');
-    if (!container) return;
-    container.innerHTML = `${numCards}/60`;
-  }
-
-  public chooseStarterDeck( selectedDeck: string) {
-    this.loadDeckFromBase64(starterDecks[selectedDeck]);
-    this.showHideStarterDeckSelector('none');
-  }
-
-  public showHideStarterDeckSelector(display: string) {
-    const container = document.getElementById('starter-deck-chooser');
-    if (!container) return;
-    container.style.display = display;
-  }
-    
-  public exportDeck() {
-    const exportCode: string = Exporter.generatePixelbornImportCode(this.cards);
-
-    const container = document.getElementById('export-window') as HTMLDivElement;
-    const textbox = document.getElementById('pixelborn-code') as HTMLTextAreaElement;
-    if (!container || !textbox) {
-      console.error('Container not found');
-      return;
+        this.updateCardRules(cards);
+        this.updateCardCounts(cards.length);
+        Stats.drawCostGraph(cards);
     }
 
-    textbox.value = exportCode;
-    container.style.display = 'block';
-  }
+    private updateCardRules(cards: Card[]): void {
+        const imageContainers = document.getElementsByClassName('ink-token') as HTMLCollectionOf<HTMLImageElement>;
+        const inkCountContainers = document.getElementsByClassName('ink-card-counter');
+        if (!imageContainers || !inkCountContainers) return;
 
-  public closeExportWindow() {
-    const container = document.getElementById('export-window') as HTMLDivElement;
-    if (!container) {
-      console.error('Container not found');
-      return;
+        const inks: string[] = this.getInks(cards);
+        let inkCounts: Record<string, number> = {};
+        cards.forEach(card => {
+            if (card.color in inkCounts) {
+                inkCounts[card.color] += 1;
+            } else {
+                inkCounts[card.color] = 1;
+            }
+        });
+
+        inks.forEach((ink, i) => {
+            imageContainers[i]!.src = inkImages[ink.toLowerCase()];
+            imageContainers[i].setAttribute('title', ink);
+            imageContainers[i].setAttribute('ink', ink.toLowerCase());
+            inkCountContainers[i].innerHTML = `${inkCounts[ink]}`;
+        });
     }
-    container.style.display = 'none';
-  }
+
+    private getInks(cards: Card[]): string[] {
+        const inks: string[] = [];
+        cards.forEach((card: Card) => {
+            if (inks.indexOf(card.color) == -1) {
+                inks.push(card.color);
+            }
+        });
+
+        return inks;
+    }
+
+    private handleDrop(event: DragEvent) {
+        this.cardDragger!.handleDropIntoDeck(event);
+    }
+
+    private handleDragOver(event: DragEvent) {
+        event.preventDefault();
+    }
+
+    public insertCard(card: Card) {
+        this.cards.push(card);
+        this.renderCards(this.cards);
+    }
+
+    public getCard(position: number): Card {
+        const card = this.cards.splice(position, 1)[0];
+        this.renderCards(this.cards);
+        return card;
+    }
+
+    private updateCardCounts(numCards: number): void {
+        const container = document.getElementById('card-count');
+        if (!container) return;
+        container.innerHTML = `${numCards}/60`;
+    }
+
+    public chooseStarterDeck(selectedDeck: string) {
+        this.loadDeckFromBase64(starterDecks[selectedDeck]);
+        this.showHideStarterDeckSelector('none');
+    }
+
+    public showHideStarterDeckSelector(display: string) {
+        const container = document.getElementById('starter-deck-chooser');
+        if (!container) return;
+        container.style.display = display;
+    }
+
+    public exportDeck() {
+        const exportCode: string = Exporter.generatePixelbornImportCode(this.cards);
+
+        const container = document.getElementById('export-window') as HTMLDivElement;
+        const textbox = document.getElementById('pixelborn-code') as HTMLTextAreaElement;
+        if (!container || !textbox) {
+            console.error('Container not found');
+            return;
+        }
+
+        textbox.value = exportCode;
+        container.style.display = 'block';
+    }
+
+    public closeExportWindow() {
+        const container = document.getElementById('export-window') as HTMLDivElement;
+        if (!container) {
+            console.error('Container not found');
+            return;
+        }
+        container.style.display = 'none';
+    }
 }
 
 (window as any).deckpage = new DeckPage();
